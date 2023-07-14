828cea1970cbf298049296550fd85e54fe456358
Tomas Sourada <souradat@u-pl23.ms.mff.cuni.cz>
2023-07-14 12:02:43 +0200

    example usage

--- example_usage.py
+++ example_usage.py
@@ -3,9 +3,24 @@
 
 from inflect import inflect
 
+# Inflecting a single lemma
+
 lemma = "jablko"
 inflected_forms = inflect(lemma)
 
 print(f"Inflected forms of lemma {lemma}:")
 for form in inflected_forms:
 	print(form)
+print()
+
+
+# Inflecting a list of lemmata
+
+lemmata = ["kočka", "pes"]
+inflected_lemmata = inflect(lemmata)
+
+for (lemma, inflected_forms) in zip(lemmata, inflected_lemmata):
+	print(f"Inflected forms of lemma {lemma}:")
+	for form in inflected_forms:
+		print(form)
+	print()
