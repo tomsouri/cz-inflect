5449f8c18b3c0e0a22b84055d19d905c6064adae
Tomas Sourada <souradat@u-pl23.ms.mff.cuni.cz>
2023-07-19 13:27:18 +0200

    example usage

--- example_usage.py
+++ example_usage.py
@@ -9,9 +9,7 @@ lemma = "diskrétka"
 inflected_forms = inflect(lemma)
 
 print(f"Inflected forms of lemma {lemma}:")
-for form in inflected_forms:
-	print(form)
-print()
+print(", ".join(inflected_forms))
 
 
 # Inflecting a list of lemmata
@@ -21,6 +19,4 @@ inflected_lemmata = inflect(lemmata)
 
 for (lemma, inflected_forms) in zip(lemmata, inflected_lemmata):
 	print(f"Inflected forms of lemma {lemma}:")
-	for form in inflected_forms:
-		print(form)
-	print()
+	print(", ".join(inflected_forms))
